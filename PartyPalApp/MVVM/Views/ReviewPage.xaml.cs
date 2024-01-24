b1dd2657dfb113ebcbb7d7db029d95a18adcaec9
schrijr <2201884schrijen@zuyd.nl>
2024-01-24 13:59:41 +0100

    Deze is voor David

--- PartyPalApp/MVVM/Views/ReviewPage.xaml.cs
+++ PartyPalApp/MVVM/Views/ReviewPage.xaml.cs
@@ -6,4 +6,12 @@ public partial class ReviewPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void OnArrow1Tapped(object sender, EventArgs e)
+    {
+        DisplayAlert("Arrow Tapped", "Arrow 1 werkt", "OK");
+    }
+	private void OnArrow2Tapped(object sender, EventArgs e)
+	{
+		DisplayAlert("Arrow Tapped", "Arrow 2 werkt", "OK");
+	}
 }
